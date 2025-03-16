4294651e05cff9bf4917ee769e7e5c8ca31dc7d2
mrko900 <mrko900@hotmail.com>
2025-03-16 12:27:42 +0300

    Tiles get_mat

---
+++ backend/tiles.py
@@ -0,0 +1,86 @@
+cnt2 = 4
+cnt = 16
+sz = 64
+
+
+def get_mat(tile):
+    mat = [[0] * cnt2 for i in range(cnt2)]
+    for i in range(cnt):
+        top = tile[i][0][sz // 2] == 255
+        bottom = tile[i][sz - 1][sz // 2] == 255
+        left = tile[i][sz // 2][0] == 255
+        right = tile[i][sz // 2][sz - 1] == 255
+        if top and left:
+            mat[0][0] = i
+        elif top and right:
+            mat[0][cnt2 - 1] = i
+        elif bottom and left:
+            mat[cnt2 - 1][0] = i
+        elif bottom and right:
+            mat[cnt2 - 1][cnt2 - 1] = i
+        elif not top and not bottom and not right and not left:
+            if tile[i][0][0] == 0:
+                mat[2][2] = i
+            elif tile[i][0][sz - 1] == 0:
+                mat[2][1] = i
+            elif tile[i][sz - 1][0] == 0:
+                mat[1][2] = i
+            else:
+                mat[1][1] = i
+    for i in range(cnt):
+        top = tile[i][0][sz // 2] == 255
+        bottom = tile[i][sz - 1][sz // 2] == 255
+        left = tile[i][sz // 2][0] == 255
+        right = tile[i][sz // 2][sz - 1] == 255
+        if top and left:
+            pass
+        elif top and right:
+            pass
+        elif bottom and left:
+            pass
+        elif bottom and right:
+            pass
+        elif not top and not bottom and not right and not left:
+            pass
+        else:
+            if top:
+                diff1 = 0
+                diff2 = 0
+                for k in range(sz):
+                    diff1 += tile[mat[0][0]][k][sz - 1] - tile[i][k][0]
+                    diff2 += tile[mat[0][3]][k][0] - tile[i][k][sz - 1]
+                if diff1 < diff2:
+                    mat[0][1] = i
+                else:
+                    mat[0][2] = i
+            elif bottom:
+                diff1 = 0
+                diff2 = 0
+                for k in range(sz):
+                    diff1 += tile[mat[3][0]][k][sz - 1] - tile[i][k][0]
+                    diff2 += tile[mat[3][3]][k][0] - tile[i][k][sz - 1]
+                if diff1 < diff2:
+                    mat[3][1] = i
+                else:
+                    mat[3][2] = i
+            elif right:
+                diff1 = 0
+                diff2 = 0
+                for k in range(sz):
+                    diff1 += tile[mat[0][0]][sz - 1][k] - tile[i][0][k]
+                    diff2 += tile[mat[3][0]][0][k] - tile[i][sz - 1][k]
+                if diff1 < diff2:
+                    mat[1][0] = i
+                else:
+                    mat[2][0] = i
+            elif left:
+                diff1 = 0
+                diff2 = 0
+                for k in range(sz):
+                    diff1 += tile[mat[0][3]][sz - 1][k] - tile[i][0][k]
+                    diff2 += tile[mat[3][3]][0][k] - tile[i][sz - 1][k]
+                if diff1 < diff2:
+                    mat[1][3] = i
+                else:
+                    mat[2][3] = i
+    return mat
--- test.py
+++ test.py
@@ -3,48 +3,8 @@ import json
 tile = []
 
 cnt = 16
+cnt2 = 4
 sz = 64
 for i in range(1, cnt + 1):
     tile.append(json.loads(open(f"backend/tyles_files/{i}.json").read()))
 
-for i in range(cnt):
-    with open(f"out/{i}.txt", "w") as f:
-        for j in range(sz):
-            f.write(str(tile[i][j]))
-            f.write("\n")
-
-# top = [-1] * cnt
-# bottom = [-1] * cnt
-# right = [-1] * cnt
-# left = [-1] * cnt
-# for i in range(0, cnt):
-#     mindiff = 1e18
-#     mindiffj = -1
-#     for j in range(0, cnt):
-#         if i == j:
-#             continue
-#         diff = 0
-#         for k in range(sz):
-#             cand = abs(tile[i][0][k] - tile[j][sz - 1][k])
-#             if cand < mindiff:
-#                 mindiff = cand
-#                 mindiffj = j
-#     top[i] = mindiffj
-#     bottom[mindiffj] = i
-# for i in range(0, cnt):
-#     mindiff = 1e18
-#     mindiffj = -1
-#     for j in range(0, cnt):
-#         if i == j:
-#             continue
-#         diff = 0
-#         for k in range(sz):
-#             cand = abs(tile[i][k][0] - tile[j][k][sz - 1])
-#             if cand < mindiff:
-#                 mindiff = cand
-#                 mindiffj = j
-#     left[i] = mindiffj
-#     right[mindiffj] = i
-#
-# for i in range(cnt):
-#     print("top", i, top[i])
