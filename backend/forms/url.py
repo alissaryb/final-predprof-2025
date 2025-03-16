9a81c2358f150f7d3a3ff8452897a61153662122
jojar <sandstorm.art@yandex.ru>
2025-03-16 12:58:05 +0300

    Url_update

--- backend/forms/url.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import InputRequired, DataRequired, ValidationError, Length, Regexp
-
-
-class FormUrl(FlaskForm):
-    url = StringField('Введите ссылку на API', validators=[DataRequired('Обязательное поле')])
-
-    submit = SubmitField('Отправить')
