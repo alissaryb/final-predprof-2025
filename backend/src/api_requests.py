f981e7831538019f963ab0c7cee49ee239e302ba
NikitaMulyar <nickm.school@mail.ru>
2025-03-16 14:12:31 +0300

    Database functions

--- backend/src/api_requests.py
+++ backend/src/api_requests.py
@@ -16,6 +16,7 @@ def get_all_tiles():
             relief = resp.json()['message']['data']
             if relief not in tyles:
                 tyles.append(relief)
+        print(resp.status_code)
     return tyles
 
 
@@ -29,4 +30,5 @@ def get_coords() -> dict:
             res['listener'] = (int(data['listener'][0]), int(data['listener'][1]))
             res['sender'] = (int(data['sender'][0]), int(data['sender'][1]))
             res['price'] = {'cuper': float(data['price'][0]), 'engel': float(data['price'][1])}
-            return data
+            return res
+        print(resp.status_code)
